a59c12f500d77c9cab82b2cd66f7c2b7ba513091
luciano-dlr <delarubialuciano@gmail.com>
2022-05-22 22:35:02 -0300

    contador

--- src/components/Contador/Contador.js
+++ src/components/Contador/Contador.js
@@ -4,18 +4,25 @@ import React , { useState } from 'react'
 const Contador = () => {
 
     
-    const [numero, setNumero] = useState(0)
+    const [numero, setNumero] = useState(1)
    
 console.log(numero)
     const mas = () =>{
+      if (numero <10){
         setNumero(numero+1)
+
+      }
     }
 
-    
+
     const menos  = () => {
-
+      if (numero > 1 ){
         setNumero(numero-1)
+      }
+    }
 
+    const reset = () =>{
+      setNumero(numero * 0)
     }
 
   return (
@@ -23,6 +30,9 @@ console.log(numero)
     <button onClick ={mas}> + </button>
     <span>{numero}</span>
     <button onClick ={menos}> - </button>
+    <div>
+      <button onClick= {reset}>Comprar</button>
+    </div>
 </div>
   )
 }
